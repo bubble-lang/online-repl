dd0422e4d7618d18d9d8f8cacf639a7c0ac9f5f2
Joe Chelladurai <58997358+joe-chelladurai@users.noreply.github.com>
2024-07-13 22:50:53 -0600

    Add

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'bubble-repl-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://raw.githubusercontent.com/bubble-lang/online-repl/main/main.wasm',
+  'https://raw.githubusercontent.com/bubble-lang/online-repl/main/wasm_exec.js',
+  // Add more files to cache as needed
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
